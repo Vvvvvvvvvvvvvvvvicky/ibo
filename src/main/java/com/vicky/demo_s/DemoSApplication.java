84e66ab3e5d38d5e50588185ea28a4313fdb5fde
vic <951386177@qq.com>
2018-07-31 16:08:30 +0800

    init

--- src/main/java/com/vicky/demo_s/DemoSApplication.java
+++ src/main/java/com/vicky/demo_s/DemoSApplication.java
@@ -1,22 +1,29 @@
 package com.vicky.demo_s;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Predicate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication // Spring Boot项目的核心注解，主要目的是开启自动配置
-@Controller // 标明这是一个SpringMVC的Controller控制器
+@SpringBootApplication 
+@Controller
 public class DemoSApplication {
 	@RequestMapping("/hello")
     @ResponseBody
     public String hello() {
         return "hello world";
     }
-
-    // 在main方法中启动一个应用，即：这个应用的入口
+	
     public static void main(String[] args) {
         SpringApplication.run(DemoSApplication.class, args);
     }
+    
 }
