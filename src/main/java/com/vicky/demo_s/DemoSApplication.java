05daf388859152cb0cfdce928ddc4b49c715abbc
vic <951386177@qq.com>
2018-08-02 17:38:03 +0800

    FreeMarker模板自动生成

--- src/main/java/com/vicky/demo_s/DemoSApplication.java
+++ src/main/java/com/vicky/demo_s/DemoSApplication.java
@@ -1,26 +1,43 @@
 package com.vicky.demo_s;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cglib.core.Predicate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.vicky.demo_s.config.UserSetting;
+
 @SpringBootApplication 
 @Controller
 public class DemoSApplication {
+	
+	@Value("${default.user.name}")
+	String defaultName;
+	@Value("${default.user.age}")
+	int defaultAge;
+	
+	@Autowired
+	private UserSetting userSetting;
+	
+	//通过@Value进行常规属性配置
 	@RequestMapping("/hello")
     @ResponseBody
     public String hello() {
-        return "hello world";
+        return "hello,"+defaultName+", you are "+defaultAge;
     }
+	
+	//测试基于properties的类型安全的配置
+	//（通过@ConfigurationProperties将properties属性和一个Bean及其属性关联）
+	@RequestMapping("/hello2")
+	@ResponseBody
+	public String hello2(){
+		return "hello,"+userSetting.getName()+", you are "+userSetting.getAge();
+	}
 	
     public static void main(String[] args) {
         SpringApplication.run(DemoSApplication.class, args);
