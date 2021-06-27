4a3d23c23a2cab3d5ad99d7f887d910e10aa89fb
vic <vicky.h.m.lee@outlook.com>
2021-06-27 12:13:36 +0800

    reset后重新提交

--- src/main/java/com/vicky/demo_s/model/Customer.java
+++ src/main/java/com/vicky/demo_s/model/Customer.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 /**
  * @className Customer
- * @desc 用来测试JPA功能而创建的，测试，加点描述，再加点描述
+ * @desc 用来测试JPA功能而创建的，测试，加点描述，reset后重新提交
  * @author Vic
  * @version 1.0
  * @date 2021/5/23 4:38 下午
